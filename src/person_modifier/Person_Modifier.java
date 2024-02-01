91c103e28d21b531e9203dad3afad3d976c54d56
unknown <nirmalawidyapuspa@gmail.com>
2024-02-01 09:09:03 +0700

    first commit

---
+++ src/person_modifier/Person_Modifier.java
@@ -0,0 +1,14 @@
+package person_modifier;
+
+public class Person_Modifier {
+
+    public static void main(String[] args) {
+        Person Munir = new Person();
+        Munir.setUsername("Nirmalawidyaa");
+        Munir.setPassword("12345678");
+        
+        System.out.println("Username : " + Munir.getUsername());
+        System.out.println("Password : " + Munir.getPassword());
+    }
+    
+}
